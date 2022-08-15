fab2f03e478094f856a291ba01b72b1066b7f157
moramadan98 <aboellamm@gmail.com>
2022-08-16 01:36:51 +0200

    add frontend

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const expressLayout = require('express-ejs-layouts');
 
 
 
@@ -10,11 +11,17 @@ const app = express()
 
 app.use(express.static(path.join(__dirname , "assets")));
 
+// بنستخدم لاي اوت دي عشان نعمل صفحة واحدة اسمها لاي اوت
+// وتبقي فيها الهيدير والفوتر والحاجات الثابتة في كل صفحة 
+// بتسهل علينا حوار اننا نعمل انكلود للهيدير والفوتر في كل صفحة
+// ملحوظة لازم نحطها قبل ما نعمل سيت للفيو انجين
+app.use(expressLayout);
 app.set('view engine' , 'ejs');
 
 
+app.use('/',require('./routes/index.route'));
 
-
+app.use('/users',require('./routes/users.route'));
 
 
 const port = process.env.PORT || 8000
